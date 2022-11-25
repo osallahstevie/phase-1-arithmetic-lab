aaa7283e452644c3e311899237895c89fb0bd40a
osallahstevie <osallahstevie@gmail.com>
2022-11-25 14:35:25 +0300

    arithmetic lab assignment completed

--- index.js
+++ index.js
@@ -1,2 +1,50 @@
 
+
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+
+const multiply = num1 * num2;
+console.log(multiply)
+
+let random = 0;
+++ random
+console.log(random)
+
+const num3 = 5;
+const num4 = 9;
+const mod = num4 % num3;
+console.log(mod)
+
+const max = Math.max(1,2,20,5)
+console.log(max)
+
+// var randomNumber = function numberGuess(){
+//     return console.log(Math.floor(Math.random() * 10) + 1);
+// }
+
+// randomNumber()
+
+// let num3 = 10;
+// let num4 = 6;
+
+// function module(num3,num4){
+
+//     return console.log(num3%num4);
+// }
+// module(10,6);
+
+
+
+// function multplier(num1,num2){
+//     if (num1 > 3) {
+//         return console.log(num1*num2)
+//     }else if(num2 != 4){
+//         console.log('You are not right')
+//     }else{
+//         console.log('I am tired')
+//     }
+    
+// }
+
+// multplier(5,4)
